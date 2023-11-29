dd2dcda4ee4bde96157f8591db6fc9ec46f16dd7
Lucie Bourreau <96539111+LucieBou@users.noreply.github.com>
2023-11-29 14:17:15 -0500

    Update the function by using the coltrane_forcing function

--- disko_example.py
+++ disko_example.py
@@ -14,58 +14,11 @@ import matplotlib.pyplot as plt
 
 from coltrane_community import coltrane_community
 from coltrane_params import coltrane_params
-from coltrane_population import coltrane_population
+from coltrane_forcing import coltrane_forcing
 
 # Forcing --------------------------------------------------------------------------------------------------       
 
-forcing = {}
-forcing['t'] = np.arange(0, 365)  # start with one year
-forcing['T0'] = np.nan * forcing['t']
-forcing['Td'] = np.nan * forcing['t']
-forcing['P'] = np.nan * forcing['t']
-
-## Surface temperature
-
-Tjan1 = -0.7  # T on Jan 1
-Tmin = -1.8  # winter min T
-Tmax = 3.7  # summer max T
-tTmin = 105  # time when spring T increase starts
-tTmax = 250  # time of T max
-nn = np.where((forcing['t']+1 >= 0) & (forcing['t']+1 <= tTmin))[0]
-forcing['T0'][nn] = np.linspace(Tjan1, Tmin, len(nn))
-nn = np.where((forcing['t']+1 >= tTmin) & (forcing['t']+1 <= tTmax))[0]
-forcing['T0'][nn] = np.linspace(Tmin, Tmax, len(nn))
-nn = np.where((forcing['t']+1 >= tTmax) & (forcing['t']+1 <= 365))[0]
-forcing['T0'][nn] = np.linspace(Tmax, Tjan1, len(nn))
-
-## Deep temperature
-
-Td0 = 1  # deep T year-round
-forcing['Td'] = np.full_like(forcing['t'], Td0)
-
-## Chlorophyll (prey)
-
-P0win = 0.1
-P0spr = 13
-P0sum = 0.05 * P0spr
-P0aut = 5
-dtPspr = 15  # bloom duration in spring
-dtPaut = 30 # bloom duration in autumn
-tPspr = 150 # time bloom initiation in spring
-tPaut = 225 # time bloom initiation in autumn
-forcing['P'] = P0win * np.ones_like(forcing['t'])
-forcing['P'] = np.maximum(forcing['P'], P0spr * np.exp(-((forcing['t']+1 - tPspr) / dtPspr) ** 2))
-forcing['P'] = np.maximum(forcing['P'], P0aut * np.exp(-((forcing['t']+1 - tPaut) / dtPaut) ** 2))
-fsum = (forcing['t']+1 > tPspr) & (forcing['t']+1 < tPaut)
-forcing['P'][fsum] = np.maximum(forcing['P'][fsum], P0sum)
-
-## Repeat this cycle for 7 years to resolve 3 year life cycles
-
-Nyears = 7
-forcing['t'] = np.arange(0, Nyears * 365)
-forcing['T0'] = np.tile(forcing['T0'], Nyears)
-forcing['Td'] = np.tile(forcing['Td'], Nyears)
-forcing['P'] = np.tile(forcing['P'], Nyears)
+forcing = coltrane_forcing("DiskoBay", 7)
 
 # Parameters and traits ------------------------------------------------------------------------------------        
 
